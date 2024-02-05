5cca39ddb5311c37081c6a71da9de46690541746
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2024-02-05 19:27:58 +0200

    Добавьте файлы проекта.

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace amazon_backend.Data
+{
+    public class DataContext:DbContext
+    {
+        // Entities: DbSet<Entity.Class> classes{get;set;}
+
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // relations
+            // seeds
+        }
+    }
+}
