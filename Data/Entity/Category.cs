e612cf964bc5fa164f92c51ccb907ccf8cb4a4b6
DDeenis <ddeenis46@gmail.com>
2024-02-07 17:10:40 +0000

    add Category entity

---
+++ Data/Entity/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace amazon_backend.Data.Entity
+{
+    public class Category
+    {
+        [Key]
+        public uint Id { get; set; }
+        public uint? ParentCategoryId { get; set; }
+        public Category? ParentCategory { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
