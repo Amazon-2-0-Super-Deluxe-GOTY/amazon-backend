a4683e2e75afa6519867090a602f8ce77f764b77
DDeenis <ddeenis46@gmail.com>
2024-02-17 13:30:30 +0000

    add user entity and user dao

--- Data/Dao/IDataAccessObject.cs
+++ Data/Dao/IDataAccessObject.cs
@@ -3,10 +3,9 @@
     public interface IDataAccessObject<T, Tid>
     {
         T[] GetAll();
-        T GetById(Tid id);
+        T? GetById(Tid id);
         void Add(T item);
         void Update(T item);
-        void Restore(Tid id);
         void Delete(Tid id);
     }
 }
---
+++ Data/Dao/UserDao.cs
@@ -0,0 +1,75 @@
+﻿using amazon_backend.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace amazon_backend.Data.Dao
+{
+    interface IUserDao : IDataAccessObject<User, string>
+    {
+        Task<User[]> GetByRoleAsync(string role);
+        Task<User[]> GetAllAsync();
+        void Restore(string id);
+    }
+
+    public class UserDao : IUserDao
+    {
+        private readonly DataContext dataContext;
+
+        public UserDao(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public void Add(User item)
+        {
+            dataContext.Users.Add(item);
+        }
+
+        public void Delete(string id)
+        {
+            User? user = dataContext.Users.Find(id);
+
+            if(user is not null)
+            {
+                user.DeletedAt = DateTime.Now;
+                dataContext.SaveChanges();
+            }
+        }
+
+        public User[] GetAll()
+        {
+            return dataContext.Users.ToArray();
+        }
+
+        public Task<User[]> GetAllAsync()
+        {
+            return dataContext.Users.ToArrayAsync();
+        }
+
+        public User? GetById(string id)
+        {
+            return dataContext.Users.Find(id);
+        }
+
+        public Task<User[]> GetByRoleAsync(string role)
+        {
+            return dataContext.Users.Where(u => u.Role == role.ToLower()).ToArrayAsync();
+        }
+
+        public void Restore(string id)
+        {
+            User? user = dataContext.Users.Find(id);
+
+            if (user is not null)
+            {
+                user.DeletedAt = null;
+                dataContext.SaveChanges();
+            }
+        }
+
+        public void Update(User item)
+        {
+            dataContext.Users.Update(item);
+            dataContext.SaveChanges();
+        }
+    }
+}
