f5859efca099e9342acc8844a257bef893f07f6e
King-Of-Programer <alexdichemix@gmail.com>
2024-02-24 01:48:34 +0200

    Сart and review

---
+++ Data/Entity/Cart.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Cart
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    //public User User { get; set; }
+    //public List<Product> Products { get; set; }
+}
