0c05e3136a8d4b078f0c53ec34b1fdc1ef1830a8
dsgnrr <semeniuk.oleg2002@gmail.com>
2024-06-04 23:23:59 +0300

    [FIX] Product creation [UPDATED] Images controllers - Now endpoints can upload file collection

--- CQRS/Commands/ReviewImageRequests/CreateReviewImageCommandRequest.cs
+++ CQRS/Commands/ReviewImageRequests/CreateReviewImageCommandRequest.cs
@@ -1,4 +1,5 @@
-﻿using amazon_backend.Models;
+﻿using amazon_backend.Data.Entity;
+using amazon_backend.Models;
 using amazon_backend.Profiles.ReviewImageProfiles;
 using FluentValidation;
 using MediatR;
@@ -7,20 +8,28 @@ using Microsoft.AspNetCore.Http;
 namespace amazon_backend.CQRS.Commands.ReviewImageRequests
 {
 
-    public class CreateReviewImageCommandRequest:IRequest<Result<ReviewImageProfile>>
+    public class CreateReviewImageCommandRequest:IRequest<Result<List<ReviewImageProfile>>>
     {
-        public IFormFile reviewImage { get; set; }
+        public List<IFormFile> reviewImages { get; set; }
     }
 
     public class CreateReviewImageValidator:AbstractValidator<CreateReviewImageCommandRequest>
     {
         public CreateReviewImageValidator()
         {
-            RuleFor(x => x.reviewImage.Length).LessThanOrEqualTo(5 * 1024 * 1024)
+            RuleFor(x => x.reviewImages)
+                .Must(reviewImages => reviewImages.Count >= 1 && reviewImages.Count <= 10)
+                .WithMessage("You must add at least 1 photo. Maximum 10 photos");
+
+            RuleForEach(x => x.reviewImages).ChildRules(reviewImage =>
+            {
+                reviewImage.RuleFor(reviewImage => reviewImage.Length).LessThanOrEqualTo(5 * 1024 * 1024)
                     .WithMessage("File size must be less than or equal to 5 MB");
-            RuleFor(x => x.reviewImage.ContentType).Must(contentType =>
-                contentType == "image/jpeg" || contentType == "image/png")
-                .WithMessage("Only JPEG and PNG files are allowed");
+
+                reviewImage.RuleFor(reviewImage => reviewImage.ContentType).Must(contentType =>
+                 contentType == "image/jpeg" || contentType == "image/png")
+                 .WithMessage("Only JPEG and PNG files are allowed");
+            });
         }
     }
 
